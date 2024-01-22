7a989bb4ea9026044a1e7aed26d166636c6e194a
SanchezrEdwin01 <sanchez.r.edwin01@gmail.com>
2024-01-22 07:07:24 -0600

    Agregar archivos de proyecto.

---
+++ IntroductionToBootstrapModals/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroductionToBootstrapModals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
